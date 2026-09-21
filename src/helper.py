5a5d14fa3435efce354a9a33f61dcb6125049469
Sanjib Nath <sanjib.nath@proton.me>
2026-09-21 22:02:24 +0530

    change the load_data function

--- src/helper.py
+++ src/helper.py
@@ -1,14 +1,6 @@
 import pandas as pd
 
-def load_data(path, as_int=True):
+def load_data(path):
     df = pd.read_csv(path)
+    return df
 
-    if as_int:
-        #getting the col header exculding the name
-        col_headers = df.columns.tolist()[1:]
-
-        #convert all the true flase into 1 and 0
-        for header in col_headers:
-            df[header] = df[header].astype(int)
-
-    return df.to_dict(orient='records')
---
+++ src/main.py
@@ -0,0 +1,3 @@
+import pandas as pd
+from decision_tree import DecisionTree
+
