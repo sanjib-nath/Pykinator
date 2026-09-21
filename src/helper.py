3d18e37ef8002fd1c29e3b9e86e9f60d2e456fe1
Sanjib Nath <sanjib.nath@proton.me>
2026-09-21 22:22:05 +0530

    ask_question function

--- src/helper.py
+++ src/helper.py
@@ -4,3 +4,17 @@ def load_data(path):
     df = pd.read_csv(path)
     return df
 
+def ask_question(feature, dataset):
+    questions = dict(zip(dataset['feature'], dataset['question']))
+    print(questions[feature])
+
+    accepted_ans = ('Y', 'N')
+ 
+    while True:
+        ans = str(input())
+
+        if ans.upper() not in accepted_ans:
+            print("Please enter [Y]es or [N]o")
+            continue
+
+        return ans
